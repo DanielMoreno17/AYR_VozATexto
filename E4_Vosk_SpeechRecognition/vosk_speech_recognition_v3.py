0ae83a88396d2c14cda7a392975b85c396fa39d7
DanielMoreno17 <luis17moreno117@gmail.com>
2025-03-19 11:02:02 -0600

    Update1

--- E4_Vosk_SpeechRecognition/vosk_speech_recognition_v3.py
+++ E4_Vosk_SpeechRecognition/vosk_speech_recognition_v3.py
@@ -1,11 +1,16 @@
 # reconocimiento_v3.py
+
 import sys
 import os
-from vosk import Model, KaldiRecognizer
-import Lexico as validador_lexico
-import pyaudio
-import SintacticoConLed
 import json
+import serial
+import time
+from vosk import Model, KaldiRecognizer
+import pyaudio
+import SintacticoConLed as sint
+
+arduino = serial.Serial("COM3", 9600, timeout=1)  
+time.sleep(2) 
 
 if not os.path.exists("model"):
     print("Por favor, descarga el modelo desde https://alphacephei.com/vosk/models y descomprímelo como 'model'.")
@@ -14,6 +19,7 @@ if not os.path.exists("model"):
 model = Model("model")
 recognizer = KaldiRecognizer(model, 16000)
 
+# Inicia el micrófono
 mic = pyaudio.PyAudio()
 stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
@@ -25,12 +31,14 @@ while True:
 
     if recognizer.AcceptWaveform(data):
         result = json.loads(recognizer.Result())  
-        temp_cadena = result.get("text", "")  
+        temp_cadena = result.get("text", "").strip() 
+        print("Comando reconocido:", temp_cadena)
 
-        print("Cadena leída:", temp_cadena)
-        print("Resultado léxico:", validador_lexico.procesar_cadena(temp_cadena))
-
-        if SintacticoConLed.valida_cadena(temp_cadena):
+        # Verificar si es un comando válido
+        if sint.valida_cadena(temp_cadena):
             print("✅ Comando válido.")
+            arduino.write((temp_cadena + "\n").encode())  
         else:
             print("❌ Comando inválido.")
+
+
